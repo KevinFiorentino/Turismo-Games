26cab7afc3717d1d68d17ad060320abe3fcabddc
KevinFiorentino <kevinfiorentino@hotmail.com>
2020-08-19 12:05:03 -0300

    Quizz 2

---
+++ Quizz/quizz2_script.js
@@ -0,0 +1,187 @@
+const myQuestions = [  
+	{
+		question: "¿Qué hitos de la historia argentina se mencionan en las cuatro caras del Obelisco?",
+		answers: {
+			a: "Puntos extremos de Argentina. Norte: Mina Pirquitas, Sur: Cabo San Pío, Este: Bernardo de Irigoyen, Oeste: PN Los Glaciares.",
+			b: "1º Fundación de Buenos Aires, 2º Fundación de Buenos Aires, 1º Izamiento de la Bandera Nacional, consolidación de Buenos Aires como capital argentina.",
+			c: "Revolución de Mayo, Homenaje a San Martín, Homenaje a Bernardino Rivadavia, centenario de la fundación de Buenos Aires."
+		},
+		correctAnswer: "1º Fundación de Buenos Aires, 2º Fundación de Buenos Aires, 1º Izamiento de la Bandera Nacional, consolidación de Buenos Aires como capital argentina.",
+		description: "La razón de su construcción fue el 500º aniversario de la primera fundación de Buenos Aires en 1536. También se conmemora la segunda fundación en 1580, el primer izamiento de la Bandera Nacional en 1812 y la consolidación de Buenos Aires como capital de la Argentina en 1880. <a target='black_' href='https://turismo.buenosaires.gob.ar/es/article/%C2%BFqu%C3%A9-%C3%ADcono-porte%C3%B1o-se-relaciona-con-egipto-un-terremoto-y-la-primera-fundaci%C3%B3n-de-buenos'>Conocé más sobre el Obelisco</a>"
+    },
+    {
+		question: "¿Cuántas especies de árboles, aves y animales terrestres se pueden avistar en la Reserva Ecológica, el espacio verde más grande de la Ciudad?",
+		answers: {
+			a: "+650 plantas y hongos, 343 aves, 906 animales terrestres.",
+			b: "+150 plantas y hongos, +200 aves, +300 animales.",
+			c: "1000 plantas y hongos, 900 aves, 700 animales terrestres."
+		},
+		correctAnswer: "+650 plantas y hongos, 343 aves, 906 animales terrestres.",
+		description: "Casi la mitad de sus plantas son típicas del Delta del Paraná y la ribera rioplatense, también hay varios ejemplares de Ceibo, la flor nacional. La Reserva es reconocida mundialmente por los amantes del avistaje de aves. <a target='blank_' href='https://turismo.buenosaires.gob.ar/es/otros-establecimientos/reserva-ecol%C3%B3gica'>Conocé más sobre la Reserva</a>"
+    },
+    {
+		question: "¿Qué película protagonizada por el estadounidense Robert Duvall fue filmada en Buenos Aires?",
+		answers: {
+			a: "Un asunto de familia (1996).",
+			b: "El Padrino (1972).",
+			c: "Assassination Tango (2002)."
+		},
+		correctAnswer: "Assassination Tango (2002).",
+		description: "La película narra la historia de un asesino a sueldo que comienza a aprender a bailar tango de la mano de la actriz argentina Luciana Pedraza, su esposa en la vida real. Figuran escenas en la tradicional milonga Sin Rumbo en Villa Urquiza, espacio en donde el propio Duvall aprendió a bailar. <a target='blank_' href='https://turismo.buenosaires.gob.ar/es/article/6-pel%C3%ADculas-filmadas-en-buenos-aires'>Conocé otras películas que transcurren en Buenos Aires</a>"
+    },
+    {
+		question: "¿Qué tango de Ástor Piazolla hace referencia a las calles de Buenos Aires?",
+		answers: {
+			a: "Libertango.",
+			b: "Adiós Nonino.",
+			c: "Balada para un loco."
+		},
+		correctAnswer: "Balada para un loco.",
+		description: "Utiliza la mención a las calles porteñas Callao y Arenales y al hospital psiquiátrico de la calle Vieytes, en Barracas, como un homenaje a la locura, tanto del amor como de la ciudad en sí misma. Las versiones más famosas fueron grabadas por Roberto Goyeneche y Amelita Baltar. <a target='blank_' href='https://turismo.buenosaires.gob.ar/es/article/5-canciones-dedicadas-buenos-aires'>Conocé qué otros músicos le cantan a Buenos Aires</a>"
+    },
+    {
+		question: "¿Con qué nombre fundó la Ciudad el colonizador Pedro de Mendoza en 1536?",
+		answers: {
+			a: "Ciudad del Espíritu Santo y Puerto de Santa María del Buen Ayre.",
+			b: "Ciudad de Trinidad.",
+			c: "Ciudad de los Buenos Ayres y del Santísimo Sacramento."
+		},
+		correctAnswer: "Ciudad del Espíritu Santo y Puerto de Santa María del Buen Ayre.",
+		description: "La Ciudad de Buenos Aires fue fundada dos veces. La primera fundación ocurrió en 1536, cuando el colonizador español Pedro de Mendoza estableció el primer asentamiento. Lo nombró Ciudad del Espíritu Santo y Puerto de Santa María del Buen Ayre. <a target='blank_' href='https://turismo.buenosaires.gob.ar/es/article/historia-de-buenos-aires'>Conocé más sobre la historia de buenos Aires</a>"
+    },
+    {
+		question: "¿Qué edificio se dice que fue construido para vengar un amor prohibido?",
+		answers: {
+			a: "Edificio Libertador.",
+			b: "Edificio Kavanagh.",
+			c: "Edificio Bencich."
+		},
+		correctAnswer: "Edificio Kavanagh.",
+		description: "Se dice que existió una relación entre Corina Kavanagh y uno de los hijos de la familia Anchorena. Los Anchorena habrían forzado la ruptura porque Corina no pertenecía a una familia patricia. Por despecho, la millonaria habría mandado a construir este enorme edificio para tapar la vista que tenían los Anchorena desde su casa a la Basílica del Santísimo Sacramento. <a target='blank_' href='https://turismo.buenosaires.gob.ar/es/otros-establecimientos/edificio-kavanagh'>Conocé más sobre la historia de este edificio</a>"
+    },
+    {
+		question: "¿Para qué construyó Rafael Díaz un chalet en la azotea de un edificio con vistas a la Av. 9 de Julio?",
+		answers: {
+			a: "Para admirar la belleza de la avenida más ancha del mundo.",
+			b: "Para almorzar y descansar.",
+			c: "Para usarla los fines de semana."
+		},
+		correctAnswer: "Para almorzar y descansar.",
+		description: "Lo construyó para dormir la siesta. Díaz vivía en Banfield y viajaba en tren hasta el centro todos los días, de manera que no tenía tiempo de volver a su casa al mediodía para comer y descansar. <a target='blank_' href='https://turismo.buenosaires.gob.ar/es/article/3-secretos-que-esconde-la-ciudad-de-buenos-aires'>Conocé otros secretos de Buenos Aires</a>"
+    },
+    {
+		question: "¿Qué reconocido arquitecto, paisajista y urbanista diagramó cerca del 80% de los espacios verdes de la Ciudad?",
+		answers: {
+			a: "Benito Javier Carrasco.",
+			b: "Fermín Eguía.",
+			c: "Carlos Thays."
+		},
+		correctAnswer: "Carlos Thays.",
+		description: "Thays era un francés que se mudó a la Argentina en 1889. Fue director de la entidad Parques y Paseos de la Ciudad de Buenos Aires. Entre sus parques más conocidos se destacan el Jardín Botánico, el Parque 3 de Febrero, Parque Lezama y Parque Centenario. <a target='blank_' href='https://turismo.buenosaires.gob.ar/es/article/4-parques-con-la-firma-de-carlos-thays'>Conocé más sobre Carlos Thays</a>"
+    },
+    {
+		question: "¿Cuántas líneas de subterráneo tiene la ciudad?",
+		answers: {
+			a: "6",
+			b: "5",
+			c: "7"
+		},
+		correctAnswer: "6",
+		description: "Buenos Aires cuenta con 6 líneas de subterráneo y una de premetro. La más antigua, primera en toda Latinoamérica y hemisferio sur, es la línea A, fundada en 1913. La más nueva, es la línea H, inaugurada en 2007. <a target='blank_' href='https://turismo.buenosaires.gob.ar/es/article/los-esp%C3%ADritus-del-subte'>Conocé qué líneas dicen tener espíritus</a>"
+    },
+    {
+		question: "¿Qué atractivo de la Ciudad fue un teatro, un cine, una disquera, una radio y una librería?",
+		answers: {
+			a: "Biblioteca Nacional.",
+			b: "Teatro San Martín.",
+			c: "Ateneo Grand Splendid."
+		},
+		correctAnswer: "Ateneo Grand Splendid.",
+		description: "Abrió sus puertas en 1919. En él tuvieron lugar conciertos de ballet, ópera y las primeras películas sonoras emitidas en Buenos Aires. Tuvo su sede el sello Nacional Odeón, hoy el sello EMI, y en los altos del edificio, comenzó su primera transmisión oficial en el año 1923 LR4 Radio Splendid. Desde el 2000 funciona una sede de la cadena de librerías El Ateneo. <a target='blank_' href='https://turismo.buenosaires.gob.ar/es/agrupador-noticias/%C2%BFqu%C3%A9-%C3%ADcono-porte%C3%B1o-se-relaciona-con%E2%80%A6'>Conocé más relaciones disfuncionales</a>"
+	},
+];
+
+var indexPreg = 0;
+var yaContestada = false;
+var correctas = 0;
+
+function turismoQuiz() {
+	if(indexPreg < myQuestions.length) {
+		yaContestada = false;
+		document.getElementById("turismo-quiz").style.display = "block";
+		document.getElementById("buttonStart").style.display = "none";
+		document.getElementById("buttonNext").style.visibility = "hidden";
+		document.getElementById("turismo-quiz").innerHTML = `
+			<div class="containerquiz">
+				<div class="question">${myQuestions[indexPreg].question}</div>
+				<ul class="listAnswers">
+					<li><strong>a. </strong><a onClick="validateAnswer(this, '${myQuestions[indexPreg].answers.a}')">${myQuestions[indexPreg].answers.a}</a></li>
+					<li><strong>b. </strong><a onClick="validateAnswer(this, '${myQuestions[indexPreg].answers.b}')">${myQuestions[indexPreg].answers.b}</a></li>
+					<li><strong>c. </strong><a onClick="validateAnswer(this, '${myQuestions[indexPreg].answers.c}')">${myQuestions[indexPreg].answers.c}</a></li>
+				</ul>
+				<div class="progress">
+				  <div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="${indexPreg+1}"
+				  	aria-valuemin="1" aria-valuemax="${myQuestions.length}" style="width:${((indexPreg+1)/myQuestions.length)*100}%">
+				  </div>
+				</div>
+				<div class="quizprogress">${indexPreg+1}/${myQuestions.length}</div>
+				<div id="quizdescrip"></div>
+			</div>
+		`;
+	}
+	else {
+		var mensaje = "";
+		document.getElementById("buttonNext").style.visibility = "hidden";
+		if(correctas <= 5) {
+			mensaje = "¡Ya sabés algunas cosas sobre Buenos Aires!";
+		}
+		else if(correctas > 5 && correctas <= 9) {
+			mensaje = "¡Lo hiciste muy bien!";
+		}
+		else if(correctas == 10) {
+			mensaje = "¡Felicitaciones! Sos un experto en la Ciudad de Buenos Aires";
+		}
+		document.getElementById("turismo-quiz").innerHTML = `
+			<div class="quizend">
+				<p>Cantidad respuestas correctas: ${correctas}/${myQuestions.length}</p>
+				<h1>¡Felicitaciones!</h1>
+				<p>${mensaje}</p>
+				<p>
+					Seguí aprendiendo sobre la 
+					<a href="https://turismo.buenosaires.gob.ar/">Ciudad de Buenos Aires</a>
+				</p>
+				<button onClick="resetQuiz()" class="btn btn-primary btn-quizreset">Jugar otra vez!</button>
+			</div>`;
+	}
+}
+
+function validateAnswer(value, answerUser) {
+	document.getElementById("buttonNext").style.visibility = "visible";
+	if(yaContestada == false) {
+		if(myQuestions[indexPreg].correctAnswer == answerUser) {
+			value.innerHTML = value.text + " " + "<span class='glyphicon glyphicon-ok'></span>";
+			value.setAttribute('style', 'color: green !important');
+			document.getElementById("quizdescrip").innerHTML = `${myQuestions[indexPreg].description}`;
+			correctas++;
+		}
+		else {
+			value.innerHTML = value.text + " " + "<span class='glyphicon glyphicon-remove'></span>";
+			value.setAttribute('style', 'color: red !important');
+			document.getElementById("quizdescrip").innerHTML = `${myQuestions[indexPreg].description}`;
+			document.querySelectorAll(".listAnswers li a").forEach(function(v) {
+				if(v.text == myQuestions[indexPreg].correctAnswer) {
+					v.setAttribute('style', 'color: green !important');
+				}
+			});
+		}
+		indexPreg++;
+	}
+	yaContestada = true;
+}
+
+function resetQuiz() {
+	indexPreg = 0;
+	yaContestada = false;
+	correctas = 0;
+	turismoQuiz();
+}
